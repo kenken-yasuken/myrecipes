1d1fca606f5264e12b6c15323b2f4ff58065121d
Kenya Yasuhara <k-yasuhara@starx.co.jp>
2018-05-11 13:40:26 +0900

    Refactor and cleean out layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,10 @@
 class RecipesController < ApplicationController
+before_action :set_recipe, only: [:show, :edit, :update]
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -22,7 +22,30 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+     end
+  end
+
+def destroy
+  Recipe.find(params[:id]).destroy
+  flash[:success] = "Recipe deleted successfully"
+  redirect_to recipes_path
+end
+
+
   private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
   def recipe_params
     params.require(:recipe).permit(:name, :description)
---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "ken", email: "ken@examplel.com")
+    @recipe = Recipe.create(name: "vegetable saute", description: "greate vegetable saute, add vegetable and oil", chef: @chef)
+
+    test "successfully delete a recipe" do
+      get recipes_path(@recipe)
+      assert_template 'recipe/show'
+      assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+      assert_difference 'Recipe.count', -1 do
+        delete recipe_path(@recipe)
+      end
+      assert_redirected_to recipes_path
+      assert_not flash.empty?
+    end
+  end
+end
